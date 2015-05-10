a40cd02de09b36979e2a74737a2e4f2d1c977535
Jaden Dessureault <jaden.dessureault@gmail.com>
2015-05-10 00:33:12 -0500

    Bundle scripts with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require('gulp'),
     util = require('gulp-util'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     templateCompiler = require('gulp-ember-template-compiler'),
     browserSync = require('browser-sync');
 
 gulp.task('serve', function() {
   browserSync.init({
-    server: './public'
+    server: './public',
+    open: false
   });
 });
 
@@ -22,16 +24,24 @@ gulp.task('styles', function() {
     .pipe(browserSync.reload({stream: true}))
 });
 
+gulp.task('scripts', function() {
+  gulp.src(['public/js/app/plum.js', 'public/js/app/**/*.js'])
+  .pipe(concat('bundle.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/js/'))
+});
+
 gulp.task('templates', function() {
   gulp.src('public/templates/**/*.hbs')
   .pipe(templateCompiler())
   .pipe(concat('templates.js'))
-  .pipe(gulp.dest('public/js/'));
+  .pipe(gulp.dest('public/js/app/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('public/scss/**/*.scss', ['styles']);
   gulp.watch('public/templates/**/*.hbs', ['templates']);
+  gulp.watch('public/js/**/*.js', ['scripts']);
 });
 
-gulp.task('default', ['templates', 'styles', 'watch', 'serve']);
+gulp.task('default', ['templates', 'styles', 'watch', 'serve', 'scripts']);
